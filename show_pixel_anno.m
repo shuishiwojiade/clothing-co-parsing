da3a29941cc638da94f6ecb041f256f7f5e3733f
shuishiwojiade <23050341+shuishiwojiade@users.noreply.github.com>
2019-05-06 19:49:24 +0800

    Add files via upload

--- show_pixel_anno.m
+++ show_pixel_anno.m
@@ -2,12 +2,14 @@ function show_pixel_anno
 % SHOW_PIXEL_ANNO visualization of pixel-level annotations
 % Wei YANG 2014
 % platero.yang (at) gmail.com
-imname = '0006';
+dirPath = './annotations/pixel-level/';
+targetPath = './output/annotations/pixel-level/';
 
+filepaths = dir(fullfile(dirPath,'*.mat'));
+for i = 1:length(filepaths)
+imname = filepaths(i).name;
 load('label_list', 'label_list'); % load label list
-load(['annotations/pixel-level/' imname '.mat'], 'groundtruth'); % load an pixel-level annotation
-
-im = imread(['photos/' imname '.jpg']);    % original image
+load(imname, 'groundtruth'); % load an pixel-level annotation
 
 % get image-level labels name
 cur_labels = unique(groundtruth);
@@ -15,14 +17,8 @@ label_names = cell(1, length(cur_labels));
 for i = 1:length(cur_labels)
     label_names(i) = label_list( cur_labels(i)+1 );
 end
-
-f = figure;
 colors = colormap( jet(length(label_list)) );   % set color map
 
-% % 1. show original photo
-subplot(1, 3, 1);  imshow(im); hold on; title('Original'); 
-
-% % 2. visualize annotation
 gt_image = zeros(size(groundtruth, 1), size(groundtruth, 2), 3);
 
 for labelidx = 1:length(cur_labels)
@@ -35,22 +31,9 @@ for labelidx = 1:length(cur_labels)
         gt_image(rows(i), cols(i), 3) = curcolor(3);
     end
 end
+% imshow(gt_image); hold on; title('Ground Truth');
+imwrite(gt_image, sprintf('%d.jpg', i), 'jpg'); 
 
-subplot(1, 3, 2); 
-imshow(gt_image); hold on; title('Ground Truth'); 
-
-% % 3. visualize legend
-subplot(1, 3, 3); 
-axis off; hold on; % show off the axis 
-for i=1:length(cur_labels)
-    [rows cols] = find(groundtruth == cur_labels(i));
-    plot(cols, rows, 's', 'MarkerEdgeColor', 'k', 'MarkerFaceColor', colors(cur_labels(i)+1, :), 'MarkerSize', 10, 'visible', 'off');
 end
-set(gca, 'Ydir', 'reverse'); hold off;
-legend(label_names, 'Location', 'West');
-pause; close all;
-end
-
-
 
 
